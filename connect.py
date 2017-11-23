085f7a05405813f8a614236c87b26070c512d825
Weichen Chen <splendidsky1117@gmail.com>
2017-11-23 11:08:48 +0800

    增加PC向手机发送命令功能

--- connect.py
+++ connect.py
@@ -1,19 +1,31 @@
 import os
 import socket
+from singleton import Singleton
 
-class Connect:
+class Connect(metaclass=Singleton):
     CMD_START_SERVICE = 'adb shell am broadcast -a CONNECT_SERVICE_START'
     CMD_STOP_SERVICE = 'adb shell am broadcast -a CONNECT_SERVICE_STOP'
     CMD_PORT_FORWARD = 'adb forward tcp:15831 tcp:15831'
 
     def __init__(self):
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
 
     def startConnect(self):
         os.system(CMD_START_SERVICE)
         os.system(CMD_PORT_FORWARD)
-        self.s.connect(('127.0.0.1', 15831))
+        self.socket.connect(('127.0.0.1', 15831))
 
     def stopConnect(self):
         os.system(CMD_STOP_SERVICE)
-        self.s.close()
+        self.socket.close()
+
+    def sendCmd(self, cmd):
+        self.socket.send(cmd)
+        buffer = []
+        while True:
+            d = self.socket.recv(1024)
+            if data:
+                buffer.append(d)
+            else:
+                break
+        data = ''.join(buffer)
